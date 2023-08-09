ca5c0a971e1d001a35bf763f8d9d6ae701fed0ed
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-10 02:26:19 +0900

    feat : lignin board origin 변경

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 const socketIO = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://board.lignin.today",
   },
 });
 
